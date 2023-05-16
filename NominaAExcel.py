bc018f18ffc8c65fdacf9cab4e17ba369ab57e11
Jesus Gonzalez <jesuslibros19@gmail.com>
2023-05-16 15:21:58 -0600

    Update NominaAExcel.py
    readFromPDF() - Read the information from the PDF
    separateTextByLines(text) - Organize the information in lines
    getUsefulLines - Filter by only the lines we need

--- NominaAExcel.py
+++ NominaAExcel.py
@@ -1,23 +1,24 @@
 from PyPDF2 import PdfReader # pip install PyPDF2
 
-# read the information from the PDF
-reader = PdfReader("test.pdf")
-number_of_pages = len(reader.pages)
+def readFromPDF():
+	# read the information from the PDF
+	reader = PdfReader("test.pdf")
+	number_of_pages = len(reader.pages)
 
-# should only have 1 page
-if(number_of_pages > 1):
-	print("[-] There is more than 1 page!")
-	exit()
+	# should only have 1 page
+	if(number_of_pages > 1):
+		print("[-] There is more than 1 page!")
+		exit()
 
-page = reader.pages[0]
-text = page.extract_text()
-
+	page = reader.pages[0]
+	text = page.extract_text()
+	return text
 
 '''
 	- input: all text from the PDF
 	- return: a list with every line of the PDF (correctly separated by line)
 '''
-def separateTextByLines():
+def separateTextByLines(text):
 	listWithLines = []
 
 	# print line by line
@@ -27,31 +28,43 @@ def separateTextByLines():
 
 		if(line == '\n'):
 			listWithLines.append(string)
-			string = ''	
-	return listWithLines
+			string = ''
 
+	return listWithLines
 
 '''
 	- input: separateTextByLines()
 	- output: a list of hashes (key-value) with the information that needs to be extracted (e.g. {001 SUELDO => 38,889.012}, {258 GASTOS MEDICOS => 453.00})
 '''
-def getUsefulInformation(lines):
+def getUsefulLines(lines):
 	# identify the line that contain "Concepto Importe" and "NETO A PAGAR"
 	# all information we need is between those lines
-	string1 = "Concepto Importe"
+
+	usefulInformation = []
+	appendInformation = False
 
 	for line in lines:
-		if string1 in line:
-			print("found it!")
-	return 1
+		if "Concepto Importe" in line:
+			appendInformation = True
 
+		if "NETO A PAGAR" in line:
+			break
+
+		if(appendInformation):
+			usefulInformation.append(line)
+
+	del usefulInformation[0] # delete line "Concepto Importe Concepto Importe"
+	return usefulInformation
 
 ''' ---------------------------------------------------------- '''
-lines = separateTextByLines()
 
-for line in lines:
-usefulInformation = getUsefulInformation(lines)
+pdfInfo = readFromPDF()
+lines = separateTextByLines(pdfInfo)
+usefulLines = getUsefulLines(lines)
 
+
+for x in usefulLines:
+	print(x)
 
 ''' 
 References:
